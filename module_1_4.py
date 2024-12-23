8e3bcba139b322e45086597b2d2ee1efbbd1d2ca
Aleksandr <moushakov@gmail.com>
2024-12-23 13:38:57 +0300

    Lesson4
    Практическая работа по уроку "Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Как ваше имя ")
+print(my_string) #Не понял какое количество символов и вывел на экран переменную my_string
+print('my_string'. upper())
+print('my_string'. lower())
+print('my_string'. replace(' ', ''))
+print('my_string'[0])
+print('my_string'[-1])
